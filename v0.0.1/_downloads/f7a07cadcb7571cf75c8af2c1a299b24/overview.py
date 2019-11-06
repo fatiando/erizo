9fd29c71f71d05c51b40630535c74736e89ef9ed
TravisCI <travis@nothing.com>
2019-11-06 18:12:23 +0000

    Deploy v0.0.1 from TravisCI

---
+++ v0.0.1/_downloads/f7a07cadcb7571cf75c8af2c1a299b24/overview.py
@@ -0,0 +1,26 @@
+"""
+.. _overview:
+
+Overview
+========
+
+Brief description of Erizo and the main
+functionality. It's a good idea to explain the package conventions.
+
+
+The library
+-----------
+
+Most classes and functions are available through the :mod:`erizo` top level package.
+
+"""
+import erizo
+
+
+###############################################################################
+# Functionality 1
+# ---------------
+#
+# Example of functionality 1. The basics.
+
+print(erizo.function())
